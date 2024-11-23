7f4eede353f5610f4b224ffda58ae790fb2dbf70
kujo205 <kuc8301@gmail.com>
2024-11-23 17:06:09 +0200

    feat: set up 2 env on coolify, added db

---
+++ src/server/db/index.ts
@@ -0,0 +1,17 @@
+import { type Database } from './types/db-types'; // this is the Database interface we defined earlier
+import { createPool } from 'mysql2'; // do not use 'mysql2/promises'!
+import { Kysely, MysqlDialect } from 'kysely';
+import { env } from '$env/dynamic/private';
+
+const dialect = new MysqlDialect({
+	// @ts-expect-error: incorrect type declarations in either mysql2 or kysely
+	pool: async () =>
+		createPool({
+			uri: env.DB_URL,
+			timezone: 'Z'
+		})
+});
+
+export const db = new Kysely<Database>({
+	dialect
+});
