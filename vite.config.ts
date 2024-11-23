a355fb46e4c686b38fd6a914315fd22e4603be2c
kujo205 <kuc8301@gmail.com>
2024-11-24 00:31:36 +0200

    chore: ignore test-results

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 export default defineConfig({
   plugins: [sveltekit()],
   test: {
+    globals: true,
+    environment: 'happy-dom',
     include: ['src/**/*.{test,spec}.{js,ts}'],
     coverage: {
       provider: 'istanbul',
