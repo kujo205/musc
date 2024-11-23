bd19bd068964bc92c78945966a976f6069eb3b6b
kujo205 <kuc8301@gmail.com>
2024-11-23 23:13:02 +0200

    ci: add code coverage

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 export default defineConfig({
   plugins: [sveltekit()],
   test: {
-    include: ['src/**/*.{test,spec}.{js,ts}']
+    include: ['src/**/*.{test,spec}.{js,ts}'],
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['text', 'json', 'html'],
+      exclude: ['node_modules', 'tests', 'build/**', '.svelte-kit/**'],
+      reportOnFailure: true
+    }
   }
 });
