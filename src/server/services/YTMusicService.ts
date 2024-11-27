4079d70eb782b9a1cbe940bfa827447affc82a4a
kujo205 <kuc8301@gmail.com>
2024-11-27 14:25:03 +0200

    chore: testing ci

--- src/server/services/YTMusicService.ts
+++ src/server/services/YTMusicService.ts
@@ -1,8 +1,6 @@
 import { BaseService } from './BaseService';
 import { db } from '$db';
 
-// import * as muse from '../../../muse/mod';
-
 class YTMusicService extends BaseService {
   constructor(db: TDatabase) {
     super(db);
