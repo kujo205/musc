4fb0584e292140e1b3543e01694934f95e845f5b
kujo205 <kuc8301@gmail.com>
2024-11-29 16:27:28 +0200

    feat: add custom function to construct absolute file path

--- src/routes/api/ping/+server.ts
+++ src/routes/api/ping/+server.ts
@@ -4,7 +4,11 @@ import { yTMusicService } from '$server/services/YTMusicService';
 export const GET: RequestHandler = async () => {
   console.log('[getting ping]');
 
-  await yTMusicService.createSharablePlaylistFromLiked('', '', '');
+  await yTMusicService.createSharablePlaylistFromLiked(
+    '',
+    'Liked Songs',
+    'Playlist of liked songs'
+  );
 
   console.log('[returning pong]');
   return text('pong');
