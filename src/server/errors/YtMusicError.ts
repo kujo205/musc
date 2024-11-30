14d92017765716650fadb7c88eea8fcff670ebb5
kujo205 <kuc8301@gmail.com>
2024-11-30 02:08:31 +0200

    feat: adde YTMusic Error

--- src/server/errors/YtMusicError.ts
+++ src/server/errors/YtMusicError.ts
@@ -2,7 +2,7 @@ import { NetworkError } from './NetworkError';
 
 export class YtMusicError extends NetworkError {
   constructor(message: string) {
-    super(message, 500);
+    super(`Error working with youtube music API: ${message} or you dont have yt channel`, 500);
     this.code = 500;
     this.name = 'YtMusicError';
   }
