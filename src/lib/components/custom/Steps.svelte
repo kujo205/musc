9ba9ec9d08ce342813aa879dec202f920b1185b2
kujo205 <kuc8301@gmail.com>
2024-12-08 22:35:17 +0200

    misc: a couple of new helpers, introduced sveltekit forms

---
+++ src/lib/components/custom/Steps.svelte
@@ -0,0 +1,21 @@
+<script lang="ts" module>
+  import type { Snippet } from 'svelte';
+
+  interface StepsProps {
+    children: Snippet;
+    className?: string;
+  }
+</script>
+
+<script lang="ts">
+  import { cn } from '$lib/utils';
+
+  const { children, className, ...rest }: StepsProps = $props();
+</script>
+
+<div
+  class={cn('[&>h3]:step steps mb-12 ml-4 border-l pl-8 [counter-reset:step]', className)}
+  {...rest}
+>
+  {@render children()}
+</div>
