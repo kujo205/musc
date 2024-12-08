289b3c436b4e9d3755c2e4439e72cd719909e730
kujo205 <kuc8301@gmail.com>
2024-12-08 22:33:34 +0200

    feat: added new routes

---
+++ src/features/account/components/CredentialsCard.svelte
@@ -0,0 +1,72 @@
+<script lang="ts" module>
+  import { type SuperValidated, type Infer } from 'sveltekit-superforms';
+  import type { TCredentialSchema } from '$features/account/schemas';
+  import { daysLeft } from '$lib/utils';
+
+  interface Props {
+    form: SuperValidated<Infer<TCredentialSchema>>;
+    expiresAt?: Date | null;
+  }
+</script>
+
+<script>
+  import { superForm } from 'sveltekit-superforms';
+  import { zodClient } from 'sveltekit-superforms/adapters';
+  import { credentialsSchema } from '$features/account/schemas';
+  import InputField from '$comp/form_fields/InputField.svelte';
+  import * as Card from '$comp/ui/card/index.js';
+  import { Button } from '$comp/ui/button/index.js';
+
+  let { form: f, expiresAt }: Props = $props();
+
+  const form = superForm(f, {
+    validators: zodClient(credentialsSchema),
+    onUpdated: ({ form: f }) => {
+      if (f.valid) {
+        // toast.success(`You submitted ${JSON.stringify(f.data, null, 2)}`);
+      } else {
+        // toast.error('Please fix the errors in the form.');
+      }
+    }
+  });
+
+  let { form: formData, enhance } = form;
+</script>
+
+<form
+  method="POST"
+  use:enhance
+  action="?/update_credentials"
+  class="min-w-72 max-sm:w-full max-sm:max-w-full"
+>
+  <Card.Root class="">
+    <Card.Header>
+      <Card.Title class="flex items-center gap-2 text-lg">Credentials</Card.Title>
+
+      <InputField
+        bind:value={$formData.set_cookie}
+        type="password"
+        name="set_cookie"
+        {form}
+        label="Response Set-Cookie header"
+      />
+
+      <InputField
+        bind:value={$formData.cookie}
+        type="password"
+        name="cookie"
+        {form}
+        label="Request Cookie header"
+      />
+    </Card.Header>
+
+    <Card.Footer class="flex justify-between gap-8">
+      <span class="text-xs text-muted-foreground">
+        {#if expiresAt}
+          Expires in {daysLeft(expiresAt)} days
+        {/if}
+      </span>
+      <Button variant="secondary" type="submit">Save</Button>
+    </Card.Footer>
+  </Card.Root>
+</form>
---
+++ src/features/account/components/MyProfileCard.svelte
@@ -0,0 +1,36 @@
+<script lang="ts" module>
+  interface MyProfileCardProps {
+    name: string | null | undefined;
+    email: string | null | undefined;
+    avatarUrl: string | null | undefined;
+  }
+</script>
+
+<script lang="ts">
+  import * as Card from '$comp/ui/card';
+
+  import * as Avatar from '$comp/ui/avatar';
+  import { getInitials } from '$lib/utils';
+
+  let { name, avatarUrl, email }: MyProfileCardProps = $props();
+</script>
+
+<Card.Root class="max-w-96 max-sm:w-full max-sm:max-w-full">
+  <Card.Content class="grid grid-cols-3 gap-x-2 gap-y-4">
+    <span class="text-muted-foreground">Name</span>
+    <p class="col-span-2 font-semibold">
+      {name}
+    </p>
+
+    <span class="text-muted-foreground">Email</span>
+    <p class="col-span-2 font-semibold">
+      {email}
+    </p>
+
+    <p class="text-muted-foreground">Avatar</p>
+    <Avatar.Root class="col-span-2">
+      <Avatar.Image src={avatarUrl} alt={name} />
+      <Avatar.Fallback>{getInitials(name)}</Avatar.Fallback>
+    </Avatar.Root>
+  </Card.Content>
+</Card.Root>
