e897a708d626e2d9ae52824be899e4a0a378b750
kujo205 <kuc8301@gmail.com>
2024-12-12 00:59:43 +0200

    fix: trying to fix routes

--- src/server/heleprs/constructAbsoluteFileName.ts
+++ src/server/heleprs/constructAbsoluteFileName.ts
@@ -1,6 +1,15 @@
 import { fileURLToPath } from 'node:url';
 import { dirname } from 'path';
 import path from 'node:path';
+import { env } from '$env/dynamic/private';
+
+export function getPythonScriptPath(scriptName: string): string {
+  if (env.ENV === 'dev') {
+    return path.resolve(process.cwd(), `static/python-scripts/${scriptName}`);
+  }
+
+  return path.resolve(process.cwd(), `build/client/python-scripts/${scriptName}`);
+}
 
 export function constructAbsoluteFileName(fileName: string, url: string): string {
   const __filename = fileURLToPath(url);
