7dbd9ab9d5480d8b21098c193e22b3d0af8dbe7c
kujo205 <kuc8301@gmail.com>
2024-12-12 00:08:12 +0200

    fix: trying to fix path problem

--- src/server/heleprs/constructAbsoluteFileName.ts
+++ src/server/heleprs/constructAbsoluteFileName.ts
@@ -1,9 +1,10 @@
 import { fileURLToPath } from 'node:url';
-import { dirname, join } from 'path';
+import { dirname } from 'path';
+import path from 'node:path';
 
 export function constructAbsoluteFileName(fileName: string, url: string): string {
   const __filename = fileURLToPath(url);
   const __dirname = dirname(__filename);
 
-  return join(__dirname, fileName);
+  return path.resolve(__dirname, fileName);
 }
