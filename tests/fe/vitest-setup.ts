731448abeea2982230a5bc072b831998d2aee8d8
kujo205 <kuc8301@gmail.com>
2024-12-16 21:44:01 +0200

    feat: add test to ChangeModeIcon

---
+++ tests/fe/vitest-setup.ts
@@ -0,0 +1,15 @@
+import { vi } from 'vitest';
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: query === '(prefers-color-scheme: light)',
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn()
+  }))
+});
