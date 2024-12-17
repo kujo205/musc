db481a900e24fb08f5795f82c3f183c1da646daa
kujo205 <kuc8301@gmail.com>
2024-12-18 01:56:16 +0200

    fix: fixed integration test setup

---
+++ tests/integration/utils/hello.test.ts
@@ -0,0 +1,7 @@
+import { expect, describe, test } from 'vitest';
+
+describe('Testing integration tests are woriking', () => {
+  test('Integration test is working', () => {
+    expect(true).toBe(true);
+  });
+});
