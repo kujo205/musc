e0d8dc9ec97cdb9d88b5bc69b4796250eef5a218
kujo205 <kuc8301@gmail.com>
2024-12-18 03:49:05 +0200

    chore: try to revive e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   webServer: {
-    command: 'npm run build && npm run preview',
+    command: 'dotenv -e .env.test npm run build && dotenv -e .env.test npm run preview',
     port: 4173
   },
 
