f80c7751f211b65d70376860326fad01a4329701
Mariia Kryvokhata <m.krivokhata@gmail.com>
2024-12-20 02:03:42 +0200

    ✨ feat: Add seeding script for testing user populatio

---
+++ src/server/db/seeds/1734651868133_testing-user-population.ts
@@ -0,0 +1,31 @@
+import type { Kysely } from 'kysely';
+
+export async function seed(db: Kysely<any>): Promise<void> {
+  await db
+    .insertInto('User')
+    .values([
+      {
+        id: crypto.randomUUID(),
+        name: 'Test',
+        email: 'test@example.com',
+        emailVerified: new Date(),
+        image: 'https://example.com/john.jpg',
+        subscription_type: 'free',
+        ytmusic_cookie: 'cookie_value_1',
+        ytmusic_cookie_expires_at: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30), // 30 days from now
+        ytmusic_set_cookie: 'set_cookie_value_1'
+      },
+      {
+        id: crypto.randomUUID(),
+        name: 'Test 2',
+        email: 'test@example.com',
+        emailVerified: new Date(),
+        image: 'https://example.com/jane.jpg',
+        subscription_type: 'basic',
+        ytmusic_cookie: 'cookie_value_2',
+        ytmusic_cookie_expires_at: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30), // 30 days from now
+        ytmusic_set_cookie: 'set_cookie_value_2'
+      }
+    ])
+    .execute();
+}
