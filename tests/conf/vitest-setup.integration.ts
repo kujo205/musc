3ecaee76c6bc113a149a489270dafa0a8179c902
Mariia Kryvokhata <m.krivokhata@gmail.com>
2024-12-20 03:12:49 +0200

    🔧 chore: update test configurations to exclude additional directories and files, also add database cleanup in integration setup script

--- tests/conf/vitest-setup.integration.ts
+++ tests/conf/vitest-setup.integration.ts
@@ -1,7 +1,11 @@
 import * as integration from './factory';
 import { beforeEach } from 'vitest';
+import { db } from '$db';
 
-beforeEach((context) => {
+beforeEach(async (context) => {
+  await db.deleteFrom('playlists').execute();
+  await db.deleteFrom('User').execute();
+
   context.integration = integration;
   context.request = new Request('http://localhost');
 });
