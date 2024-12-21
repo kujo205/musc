50ad58f0c8eb18ad9acc1dab0d276f0c2ca16fc2
Mariia Kryvokhata <m.krivokhata@gmail.com>
2024-12-21 18:23:23 +0200

    chore: finally make integraion tests work

--- tests/conf/vitest-setup.integration.ts
+++ tests/conf/vitest-setup.integration.ts
@@ -1,10 +1,8 @@
 import * as integration from './factory';
-import { beforeEach } from 'vitest';
-import { db } from '$db';
+import { beforeEach, afterEach } from 'vitest';
 
 beforeEach(async (context) => {
-  await db.deleteFrom('playlists').execute();
-  await db.deleteFrom('User').execute();
+  await integration.teardown();
 
   context.integration = integration;
   context.request = new Request('http://localhost');
