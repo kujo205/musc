a5ebf6de42451017ae50bbc7d5d37b5a89270d39
kujo205 <kuc8301@gmail.com>
2024-12-22 16:31:34 +0200

    ✨ feat: Update environment to 'dev', add UI test command, and introduce button test ids
    This commit includes the following changes:
    - Updated environment variable to 'dev' in .env.test
    - Added a new UI test command in package.json
    - Introduced button test ids in PlanCard.svelte components
    - Included a new import for Credentials in h0_auth.ts
    - Added console log statement for debugging in h1_locals_population.ts
    - Introduced button test ids in +page.svelte route file
    - Expanded e2e test coverage for marketing page with redirection validation

--- tests/e2e/pages/main.test.ts
+++ tests/e2e/pages/main.test.ts
@@ -1,10 +1,37 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('/');
+test.describe('Marketing page', () => {
+  test('Loads main page with correct title', async ({ page }) => {
+    await page.goto('/');
 
-  // Log the page title for debugging
-  const title = await page.title();
+    const title = await page.title();
 
-  expect(title).toBe('Musc');
+    expect(title).toBe('Musc');
+  });
+
+  test('Redirects to signup when unauthorized user clicks basic plan card', async ({
+    page,
+    context
+  }) => {
+    await page.goto('/');
+
+    await context.clearCookies();
+    await page.reload({ waitUntil: 'networkidle' });
+
+    // sign-up-free
+    const basicPlanBtn = page.getByTestId('sign-up-basic');
+
+    console.log(basicPlanBtn);
+
+    await basicPlanBtn.click();
+
+    await page.waitForTimeout(10000);
+    // await page.waitForURL('https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount**');
+
+    const url = page.url();
+
+    console.log(url);
+
+    expect(url).toContain('localhost');
+  });
 });
