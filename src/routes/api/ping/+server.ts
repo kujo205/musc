bfd9154f9c34eaf9d9a4f49e66a580709f712704
kujo205 <kuc8301@gmail.com>
2024-12-28 19:37:00 +0200

    ♻️ refactor: cleanup unused code, sync music and add new endpoint

--- src/routes/api/ping/+server.ts
+++ src/routes/api/ping/+server.ts
@@ -4,9 +4,6 @@ import { ytMusicController } from '$server/controllers/YtMusicController';
 export const GET: RequestHandler = async () => {
   console.log('[getting ping]');
 
-  // await ytMusicController.createSharablePlaylistFromLiked('kuc8301@gmail.com', 'Test Playlist');
-  await ytMusicController.syncMusic();
-
   console.log('[returning pong]');
   return text('pong');
 };
