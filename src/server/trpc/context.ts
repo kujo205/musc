05e0b6540c28f2a4c90006f402bd45f2a559d3b2
kujo205 <kuc8301@gmail.com>
2025-01-25 02:26:13 +0200

    ✨ feat: Add server-side TypeScript files and functions for handling TRPC requests and router definition

---
+++ src/server/trpc/context.ts
@@ -0,0 +1,11 @@
+import type { RequestEvent } from '@sveltejs/kit';
+
+export function createContext(event: RequestEvent) {
+  return {
+    cookies: event.cookies,
+    request: event.request,
+    ...event.locals
+  };
+}
+
+export type Context = ReturnType<typeof createContext>;
