a54309811e2310be3b4f95c5d9b754a62b99bf1a
kujo205 <kuc8301@gmail.com>
2025-01-28 12:23:13 +0200

    chore: add trpc

--- src/server/trpc/context.ts
+++ src/server/trpc/context.ts
@@ -1,11 +1,11 @@
 import type { RequestEvent } from '@sveltejs/kit';
 
 export function createContext(event: RequestEvent) {
-  return {
+  return () => ({
     cookies: event.cookies,
     request: event.request,
     ...event.locals
-  };
+  });
 }
 
 export type Context = ReturnType<typeof createContext>;
