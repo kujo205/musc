76192d1a837f4f074026f3512874a0e347d53616
kujo205 <kuc8301@gmail.com>
2025-02-03 00:03:47 +0200

    chore: fix e2e test

---
+++ src/routes/(payment)/checkout/failure/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Button } from '$comp/ui/button';
+  import Error from '$comp/svgs/Error.svelte';
+</script>
+
+<div class="flex min-h-[50vh] flex-col items-center justify-center gap-4">
+  <Error className="max-w-96 w-fit aspect-square max-h-96" />
+
+  <div class="flex flex-col items-center justify-center">
+    <h2 class="mb-2 text-2xl font-semibold">What a misfortune</h2>
+    <p class="text-center">
+      Due to some circumstances, we were unable to conduct a payment, please contact the support.
+    </p>
+  </div>
+
+  <Button href="mailto:m.krivokhata@gmail.com">Consult support</Button>
+</div>
